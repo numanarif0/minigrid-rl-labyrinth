6a72e23eb45ca8448ea9476d3481cc817f2f0bf3
numanarif0 <numanarifdeniz@gmail.com>
2026-06-09 17:50:39 +0300

    added requirements

--- tensorboard_compare.ps1
+++ tensorboard_compare.ps1
@@ -1,2 +1,2 @@
 $root = Split-Path -Parent $MyInvocation.MyCommand.Path
-tensorboard --logdir_spec "PPO:$root\ppo_tensorboard,DQN:$root\dqn_tensorboard"
+tensorboard --logdir_spec "PPO_V1:$root\ppo_tensorboard,PPO_V2:$root\ppo_tensorboard_v2,DQN_V2:$root\dqn_tensorboard_v2,DQN_V3:$root\dqn_tensorboard_v3"
